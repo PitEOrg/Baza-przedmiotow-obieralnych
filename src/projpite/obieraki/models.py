1dfc1e3d0bab3167d5ea41828077ba4f1574280b
Wicked <lukasz.ujma.94@gmail.com>
2015-06-02 20:33:34 +0200

    Signed-off-by: Wicked <lukasz.ujma.94@gmail.com>
    Porządkujące zmiany w bazie danych

--- src/projpite/obieraki/models.py
+++ src/projpite/obieraki/models.py
@@ -1,48 +1,56 @@
 from django.db import models
 
 #Create your models here.
+# Zakomentowane zostały elementy które przy odbiorze projektu uznano za niepotrzebne
+# W razie konieczności można je przywrócić do działania
 
 class Class_Type(models.Model):
     Name = models.TextField()
     MinStudents = models.IntegerField()
     MaxStudents = models.IntegerField()
 
+
 class User_2(models.Model):
-    Name = models.TextField()
-    Surname = models.TextField()
-    Mail = models.TextField(unique=True)
     Login = models.TextField(unique=True)
     Password_2 = models.TextField()
+    Mail = models.TextField(unique=True)
     Permission = models.IntegerField()
+    #Name = models.TextField()
+    #Surname = models.TextField()
     
     
 class Student(models.Model):
     User_2_idUser_2 = models.ForeignKey(User_2)
+    Name = models.TextField()
+    Surname = models.TextField()
     FieldOfStudy = models.TextField()
     Year_2 = models.IntegerField()
     Semester = models.IntegerField()
+    CollectedECTS = models.IntegerField()
+
 
 class Staff(models.Model):
     User_2_idUser_2 = models.ForeignKey(User_2)
-    Department = models.TextField()
+    Name = models.TextField()
+    Surname = models.TextField()
     Title = models.TextField()
+    Department = models.TextField()
 
 
 class Course(models.Model):
-    Staff_idStaff = models.ForeignKey(User_2)
     Name = models.TextField()
-    ID_NO = models.IntegerField()
+    Staff_idStaff = models.ForeignKey(User_2)
     ECTS = models.IntegerField()
-    Desription = models.TextField()
-    Hours = models.IntegerField()
     MinStudents = models.IntegerField()
     MaxStudents = models.IntegerField()
     Semester = models.IntegerField()
-
-
-class Student_has_Course(models.Model):
-    Student_idStudent = models.ForeignKey(Student)
-    Course_idCourse = models.ForeignKey(Course)
+    Desription = models.TextField()
+    Requirements = models.TextField()
+    WayOfGettingCredit = models.TextField()
+    Requirements = models.TextField()
+    Exam = models.BooleanField()
+    #Hours = models.IntegerField()
+    #ID_NO = models.IntegerField()
 
 
 class Class(models.Model):
@@ -50,9 +58,17 @@ class Class(models.Model):
     Class_Type_idClass = models.ForeignKey(Class_Type)
     Staff_idStaff = models.ForeignKey(Staff)
     Hours = models.IntegerField()
-    Instructor = models.TextField()
+    #Instructor = models.TextField()
+
+
+class Student_has_Course(models.Model):
+    Student_idStudent = models.ForeignKey(Student)
+    Course_idCourse = models.ForeignKey(Course)
+
 
 class Student_has_Class(models.Model):
     Student_idStudent = models.ForeignKey(Student)
     Class_Course_idCourse = models.ForeignKey(Course)
     Class_Class_Type_idClass = models.ForeignKey(Class_Type)
+
+
