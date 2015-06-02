33eac5704d1c6d72dbac3bfd99755882a95917bd
Wicked <lukasz.ujma.94@gmail.com>
2015-06-02 23:18:24 +0200

    Database - Małe zmiany w bazie danych - Dodane przykładowe rekordy do bazy
    Wicked <lukasz.ujma.94@gmail.com>

--- src/projpite/obieraki/admin.py
+++ src/projpite/obieraki/admin.py
@@ -3,7 +3,11 @@ from .models import *
 
 admin.site.register(User_2)
 admin.site.register(Student)
+admin.site.register(Staff)
 admin.site.register(Course)
 admin.site.register(Class)
+admin.site.register(Class_Type)
+admin.site.register(Student_has_Course)
+admin.site.register(Student_has_Class)
 
 # Register your models here.
--- src/projpite/obieraki/models.py
+++ src/projpite/obieraki/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
 #Create your models here.
-# Zakomentowane zostały elementy które przy odbiorze projektu uznano za niepotrzebne
-# W razie konieczności można je przywrócić do działania
-
-class Class_Type(models.Model):
-    Name = models.TextField()
-    MinStudents = models.IntegerField()
-    MaxStudents = models.IntegerField()
-
+# Zakomentowane zostaly elementy ktore przy odbiorze projektu uznano za niepotrzebne
+# W razie koniecznosci mozna je przywrocic do dzialania
+# TO DO: Mozliwosc stworzenia studenta bez stworzonego jeszcze konta dla niego
 
 class User_2(models.Model):
     Login = models.TextField(unique=True)
@@ -17,6 +12,8 @@ class User_2(models.Model):
     Permission = models.IntegerField()
     #Name = models.TextField()
     #Surname = models.TextField()
+    def __str__(self):
+        return self.Mail
     
     
 class Student(models.Model):
@@ -27,6 +24,8 @@ class Student(models.Model):
     Year_2 = models.IntegerField()
     Semester = models.IntegerField()
     CollectedECTS = models.IntegerField()
+    def __str__(self):
+        return self.Name + ' ' + self.Surname
 
 
 class Staff(models.Model):
@@ -35,11 +34,13 @@ class Staff(models.Model):
     Surname = models.TextField()
     Title = models.TextField()
     Department = models.TextField()
+    def __str__(self):
+        return self.Name + ' ' + self.Surname
 
 
 class Course(models.Model):
     Name = models.TextField()
-    Staff_idStaff = models.ForeignKey(User_2)
+    Staff_idStaff = models.ForeignKey(Staff)
     ECTS = models.IntegerField()
     MinStudents = models.IntegerField()
     MaxStudents = models.IntegerField()
@@ -51,6 +52,16 @@ class Course(models.Model):
     Exam = models.BooleanField()
     #Hours = models.IntegerField()
     #ID_NO = models.IntegerField()
+    def __str__(self):
+        return self.Name
+
+
+class Class_Type(models.Model):
+    Name = models.TextField()
+    MinStudents = models.IntegerField()
+    MaxStudents = models.IntegerField()
+    def __str__(self):
+        return self.Name
 
 
 class Class(models.Model):
@@ -59,16 +70,22 @@ class Class(models.Model):
     Staff_idStaff = models.ForeignKey(Staff)
     Hours = models.IntegerField()
     #Instructor = models.TextField()
+    def __str__(self):
+        return self.Course_idCourse.Name + ' - ' + self.Class_Type_idClass.Name
 
 
 class Student_has_Course(models.Model):
     Student_idStudent = models.ForeignKey(Student)
     Course_idCourse = models.ForeignKey(Course)
+    def __str__(self):
+        return self.Student_idStudent.Name + ' ' + self.Student_idStudent.Surname + ' - ' + self.Course_idCourse.Name
 
 
 class Student_has_Class(models.Model):
     Student_idStudent = models.ForeignKey(Student)
     Class_Course_idCourse = models.ForeignKey(Course)
     Class_Class_Type_idClass = models.ForeignKey(Class_Type)
+    def __str__(self):
+        return self.Student_idStudent.Name + ' ' + self.Student_idStudent.Surname + ' - ' + self.Class_Course_idCourse.Name + ' - ' + self.Class_Class_Type_idClass.Name
 
 
